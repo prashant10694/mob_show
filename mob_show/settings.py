ea1b832b8a8c8f7b615ce19bc620202e9fbf2282
prashant10694 <prashant21110694@gmail.com>
2016-05-26 00:17:01 +0530

    first commit

--- mob_show/settings.py
+++ mob_show/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mob_show'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'mob_show.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE' : 'django_mongodb_engine',
+        'NAME' : 'phone_world',
     }
 }
 
--- phonearena/models.py
+++ phonearena/models.py
@@ -1,5 +1,82 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from djangotoolbox.fields import EmbeddedModelField
 
 # Create your models here.
+
+class Smartphone(models.Model):
+	name = models.CharField(max_length=200, db_index=True )
+	technology = models.CharField(default='', blank=True, null=True, max_length= 100)
+	launch = EmbeddedModelField('Launch')
+	body = EmbeddedModelField('Body')
+	display = EmbeddedModelField('Display')
+	platform = EmbeddedModelField('Platform')
+	memory = EmbeddedModelField('Memory')
+	camera = EmbeddedModelField('Camera')
+	sound = EmbeddedModelField('Sound')
+	comms = EmbeddedModelField('Comms')
+	features = EmbeddedModelField('Features')
+	battery = models.CharField(default='', blank=True, null=True, max_length= 100)
+	misc = 
+
+class Launch(models.Model):
+	announced = models.CharField()
+	status = models.CharField()
+
+
+class Body(models.Model):
+	dimension = models.CharField()
+	weight = models.CharField()
+	build = models.CharField()
+	sim_type = models.CharField()
+
+
+class Display(models.Model):
+	display_type = models.CharField()
+	size = models.CharField()
+	resolution = models.CharField()
+	multitouch = models.CharField()
+	protection = models.CharField()
+
+
+class Platform(models.Model):
+	os = models.CharField()
+	chipset = models.CharField()
+	cpu = models.CharField()
+	gpu = models.CharField()
+
+
+class Memory(models.Model):
+	card_slot = models.CharField()
+	internal = models.CharField()
+
+
+class Camera(models.Model):
+	primary = models.CharField()
+	features = models.CharField()
+	video = models.CharField()
+	secondary = models.CharField()
+
+
+class Sound(models.Model):
+	alert_types = models.CharField()
+	loudspeaker = models.CharField()
+	jack = models.CharField()
+
+
+class Comms(models.Model):
+	wlan = models.CharField()
+	bluetooth = models.CharField()
+	gps = models.CharField()
+	nfc = models.CharField()
+	radio = models.CharField()
+	usb = models.CharField()
+
+
+class Features(models.Model):
+	sensors = models.CharField()
+	messaging = models.CharField()
+	browser = models.CharField()
+	java = models.CharField()
+	extra = EmbeddedModelField('Extra')
